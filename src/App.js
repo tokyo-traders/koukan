d0cf9daeca3b61961530229017bfeea4eeec51a2
Adam Burrough <adamjamesburrough@gmail.com>
2022-12-21 19:28:27 +0900

    patch: fix bug

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { useEffect} from "react";
+
+
 
 function App() {
+  
+  useEffect(()=>{
+    axios
+    .get(`/api`)
+    .then((response) => {
+       console.log(response.data)
+    })
+  }, [])
+
+  
   return (
     <div className="App">
-    Hello!
+    Hello! test
 
     </div>
   );
