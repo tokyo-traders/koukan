7fe93607363c4e6a3bb2c9260b3bcc9b92b73fba
Yamaki09 <kojie09yamashita@gmail.com>
2022-12-22 22:37:27 +0900

    feat: CRUD user

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import UserSerializer, CreateUserSerializer
+from .serializers import UserSerializer
 from .models import User
 
 @api_view(['GET', 'POST'])
@@ -13,6 +13,32 @@ def user_list(request):
         obj = User.objects.all()
         serializer = UserSerializer(obj, many = True)
         return Response(serializer.data)
+    if request.method == "POST":
+        serializer = UserSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_edit(request, id):
+
+    try:
+        user = User.object.get(pk = id)
+    except User.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+    
+    if request.method == "GET":
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = UserSerializer(user, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_200_OK)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(['GET', 'POST'])
