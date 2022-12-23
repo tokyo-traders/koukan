5d92a68f12919f05a98b57e4c144975ee8b457ab
rpiflv <rpiflv@gmail.com>
2022-12-23 20:00:59 +0900

    add redux store

--- src/componenet/Sidebar.js
+++ src/componenet/Sidebar.js
@@ -1,15 +1,31 @@
 import React, { useEffect, useState } from "react";
 import "./Sidebar.css"
 
-
 function Sidebar() {
 
     return (
         <>
-            <div class="sidebar">
-                <div>Menu Item 1</div>
-                <div>Menu Item 2</div>
-                <div>Menu Item 3</div>
+            <div class="side-bar">
+                <div class="menu">
+                    <div class="item"><a href="#"><i class="fas fa-desktop"></i>Dashboard</a></div>
+                    <div class="item">
+                        <a class="sub-btn"><i class="fas fa-table"></i>Tables<i class="fas fa-angle-right dropdown"></i></a>
+                        <div class="sub-menu">
+                            <a href="#" class="sub-item">Sub Item 01</a>
+                            <a href="#" class="sub-item">Sub Item 02</a>
+                            <a href="#" class="sub-item">Sub Item 03</a>
+                        </div>
+                    </div>
+                    <div class="item"><a href="#"><i class="fas fa-th"></i>Forms</a></div>
+                    <div class="item">
+                        <a class="sub-btn"><i class="fas fa-cogs"></i>Settings<i class="fas fa-angle-right dropdown"></i></a>
+                        <div class="sub-menu">
+                            <a href="#" class="sub-item">Sub Item 01</a>
+                            <a href="#" class="sub-item">Sub Item 02</a>
+                        </div>
+                    </div>
+                    <div class="item"><a href="#"><i class="fas fa-info-circle"></i>About</a></div>
+                </div>
             </div>
         </>
     )
