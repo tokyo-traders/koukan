06eda29c030df31bfb31aec72cb52c4f5b3436a3
Evone <yfh910@gmail.com>
2022-12-24 16:48:06 +0900

    LoginForm WIP

--- src/componenet/Registration/LoginForm.js
+++ src/componenet/Registration/LoginForm.js
@@ -1,16 +1,42 @@
 import {useRef, useState, useEffect } from "react";
+import axios from "axios";
 
 
 function LoginForm({ Login, error }) {
   const [details, setDetails] = useState({email:"", password:""});
 
+  // const name = axios.get("api/all"
+  //   .then((res) => {
+  //     console.log(res)
+  //    })
+  //   .catch((err) => alert("error!")))
+
   const submitHandler = e => {
     e.preventDefault();
-    Login(details)
+    // console.log(e)
+    // Login(details)
+    
+    // try {
+    //   const response = await axios.post('api/user/login', 
+    //   JSON.stringfy({email:details.email,password:details.password})
+    //   )
+    //   console.log(JSON.stringify)
+    // }
+    console.log({email:details.email, password:details.password})
+
+    axios
+    .post('/api/user/login', 
+    JSON.stringify({email:details.email, password:details.password})
+    )
+    .then((res) => {
+      console.log(res)
+     })
+    .catch((err) => alert("error!"));
   }
 
   return (
     <form onSubmit={submitHandler}>
+      {% csrf_token %}
       <div className="form-inner">
         <h2>Login</h2> 
         {/*ERROR! */}
