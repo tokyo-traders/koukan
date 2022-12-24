c5d54ba0613dd736a4fa7251876f24e16c96c5c6
Yamaki09 <kojie09yamashita@gmail.com>
2022-12-24 14:29:37 +0900

    feat: CRUD in item table

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,15 +3,16 @@ from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import UserSerializer
-from .models import User
+from .serializers import UserSerializer, ItemSerializer
+from .models import User, Item
 
 @api_view(['GET', 'POST'])
 def user_list(request):
-    
-    
+ 
     if request.method == "GET":
         obj = User.objects.all()
+        item = Item.objects.all()
+        print(item)
         serializer = UserSerializer(obj, many = True)
         return Response(serializer.data)
     if request.method == "POST":
@@ -20,11 +21,47 @@ def user_list(request):
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
 
+
+@api_view(['GET', 'POST'])
+def item_list(request):
+
+    if request.method == "GET":
+        item = Item.objects.all()
+        serializer = ItemSerializer(item, many = True)
+        return Response(serializer.data)
+    if request.method == "POST":
+        serializer = ItemSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE', 'POST'])
+def item_edit(request, name):
+
+    try:
+        item = Item.objects.get(item_name = name)
+    except User.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+
+    
+    if request.method == "GET":
+        serializer = UserSerializer(item)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = ItemSerializer(item, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_200_OK)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 @api_view(['GET', 'PUT', 'DELETE', 'POST'])
 def user_edit(request, name):
 
     try:
-        user = User.objects.get(username = name)
+        user = User.objects.get(first_name = name)
     except User.DoesNotExist:
         return Response(status = status.HTTP_404_NOT_FOUND)
 
@@ -41,6 +78,7 @@ def user_edit(request, name):
     elif request.method == 'DELETE':
         user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 
 @api_view(['GET', 'POST'])
