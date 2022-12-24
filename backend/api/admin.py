c815e0531f49e29455ae0a7e8aab9a998bff6dd1
Yamaki09 <kojie09yamashita@gmail.com>
2022-12-24 11:55:22 +0900

    feat: item table

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from api.models import User
+from api.models import User, Item
 
 class UserAdmin(admin.ModelAdmin):
     pass
-  
+
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(User, UserAdmin)
+admin.site.register(Item, ItemAdmin)
