72ef55d9e62c6b5f7dad205d59f2a156fd432b1f
Yamaki09 <kojie09yamashita@gmail.com>
2022-12-27 14:26:56 +0900

    feat: item table adding images

--- backend/api/views.py
+++ backend/api/views.py
@@ -21,6 +21,28 @@ def user_list(request):
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
 
+@api_view(['GET', 'PUT', 'DELETE', 'POST'])
+def user_edit(request, name):
+
+    try:
+        user = User.objects.get(first_name = name)
+    except User.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+
+    
+    if request.method == "GET":
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = UserSerializer(user, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_200_OK)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 @api_view(['GET', 'POST'])
 def item_list(request):
@@ -56,38 +78,6 @@ def item_edit(request, name):
     elif request.method == 'DELETE':
         item.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-@api_view(['GET', 'PUT', 'DELETE', 'POST'])
-def user_edit(request, name):
-
-    try:
-        user = User.objects.get(first_name = name)
-    except User.DoesNotExist:
-        return Response(status = status.HTTP_404_NOT_FOUND)
-
-    
-    if request.method == "GET":
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
-    elif request.method == "POST":
-        serializer = UserSerializer(user)
-        userPassword = serializer.data["password"]
-        userEmail = serializer.data["email"]
-        password = request.data["password"]
-        email = request.data["email"]
-        if userPassword == password and userEmail == email:
-            return Response(True, status=status.HTTP_200_OK)
-        return Response(False, status=status.HTTP_401_UNAUTHORIZED)
-    elif request.method == "PUT":
-        serializer = UserSerializer(user, data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status = status.HTTP_200_OK)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        user.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
 
 
 @api_view(['GET', 'POST'])
