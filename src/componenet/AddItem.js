1187ece88d49f7c56c604071357638e1abca184b
rpiflv <rpiflv@gmail.com>
2023-01-05 09:37:27 +0900

    multiple image

--- src/componenet/AddItem.js
+++ src/componenet/AddItem.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
-
+import _ from "lodash";
 import './AddItem.css';
 import { upload } from '@testing-library/user-event/dist/upload';
 
@@ -8,36 +8,49 @@ import { upload } from '@testing-library/user-event/dist/upload';
 function AddItem() {
 
     const [itemName, setItemName] = useState("");
-    const [itemImage, setItemImage] = useState();
+    const [itemImageObj, setItemImageObj] = useState([]);
+    const [itemImageArr, setItemImageArr] = useState([]);
+
     const [details, setDetails] = useState('');
     const [desire, setDesire] = useState('');
 
+
+    const uploadData = new FormData();
     const newItem = async () => {
-        const uploadData = new FormData();
         uploadData.append('item_name', itemName);
-        uploadData.append('item_image', itemImage);
+        uploadData.append('item_image', itemImageArr);
         uploadData.append('details', details);
         uploadData.append('user_id', 1);
-
         let newItem = await fetch('/api/item', {
             method: 'POST',
             body: uploadData
         })
             .then(res => res.json())
             .catch(error => console.log(error))
-
-        console.log(newItem)
-        setItemImage();
+        // for (let key in uploadData) {
+        //     console.log(key);
+        // }
+        // console.log(itemImageArr)
+        setItemImageArr([]);
         setDesire('');
         setDetails('');
         setItemName('');
 
     }
 
+    const handleChange = (e) => {
+        // let imgs = []
+        _.forEach(e.target.files, file => {
+            uploadData.append('item_image', file)
+        })
+        setItemImageArr(itemImageArr)
+    }
+
     return (
         <div className="App">
             <h3>Add an Item</h3>
             <label>
+
                 Title
                 <input type="text" value={itemName} onChange={(e) => setItemName(e.target.value)} />
             </label>
@@ -54,8 +67,9 @@ function AddItem() {
             <br />
             <label>
                 Image
-                <input type="file" onChange={(e) => setItemImage(e.target.files[0])} />
+                <input type="file" onChange={handleChange} multiple />
             </label>
+            <img src={itemImageArr?.url} />
             <br />
             <button onClick={() => newItem()}>New item</button>
         </div>
