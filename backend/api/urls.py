3a919f6d3a6e686fe6184c336678329bacab12b7
Yamaki09 <kojie09yamashita@gmail.com>
2023-01-11 22:23:34 +0900

    feat: path urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import user_list, user_edit, item_list, hello
+from .views import user_list, user_edit, item_list, image_list, hello
 from api import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -10,14 +10,14 @@ router = routers.DefaultRouter()
 router.register('image', views.ImageView)
 router.register('image/multiple_upload', views.ImageView)
 
-
 urlpatterns = [
     path('user/all', views.user_list),
     path('user/login', views.user_edit),
-    path('item', views.item_list),
-    path('item/<str:name>', views.item_edit),
+    path('item/<int:userid>', views.item_list), # to GET and POST all the item objects
+    path('item/<str:username>/<int:id>', views.item_edit), # to get the item with full CRUD
+    path('item-image/<int:itemId>', views.image_list), # to get the images path with GET and DELETE method only
     path('hello', views.hello),
-    path('user/<str:name>', views.user_edit),
+    path('user/login/<str:name>', views.user_edit),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #
 # use the static or...
