27078dd8f2d9ef0b1b49fbdb98056051a7f3f635
rpiflv <rpiflv@gmail.com>
2023-01-20 11:54:09 +0900

    render post from DB (item name test OK)

--- src/componenet/AllListings.js
+++ src/componenet/AllListings.js
@@ -4,14 +4,12 @@ import React, { useState, useEffect } from 'react';
 const BASE_URL = 'http://127.0.0.1:8000/api';
 
 export default function AllListings() {
-    const [listings, setListings] = useState([{
-    }]);
+    const [listings, setListings] = useState([]);
 
     useEffect(() => {
         axios
             .get('api/homepage')
-            .then(response => console.log(response.data))
-            .then(res => setListings(res.data))
+            .then(res => setListings(...listings, res.data))
     }, [])
     console.log(listings)
     return (
@@ -19,7 +17,7 @@ export default function AllListings() {
             <h1>All listings</h1>
             {listings.map(listing => (
 
-                < h3 > {listing.id}</h3>
+                < h3 > {listing.item[0].item_name}</h3>
             ))
             }
         </>
