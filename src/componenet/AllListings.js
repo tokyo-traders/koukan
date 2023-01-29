f48e1cf82bcc5108a503c06b1c5dc97590ffc948
Adam Burrough <adamjamesburrough@gmail.com>
2023-01-30 05:45:53 +0900

    div alignment

--- src/componenet/AllListings.js
+++ src/componenet/AllListings.js
@@ -1,4 +1,10 @@
 import { width } from '@mui/system';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -31,15 +37,33 @@ export default function AllListings(props) {
 
     const showListing = (listing) => {
         return (
-            <div className='listingClass'
+            // <div className='listingClass'
+            //     >
+            //     <img alt="image1" width="220px" src={BASE_URL + `${listing?.images[0]}`} />
+            //     < h3 > {listing.item.item_name}</h3>
+
+                <Card 
+                elevation={2} 
+                sx={{ maxWidth: 200, mt: 5, marginLeft: 3}}
                 onClick={() => {
                     if (listing) {
                         makeOffer(listing)
                     }
-                }}>
-                <img alt="image1" width="220px" src={BASE_URL + `${listing?.images[0]}`} />
-                < h3 > {listing.item.item_name}</h3>
-            </div>
+                }}
+                >
+                    <CardMedia
+                        component="img"
+                        image={BASE_URL + `${listing?.images[0]}`}
+                        width="200"
+                        sx={{ padding: "1em 1em 0 1em", objectFit: "contain" }}
+                        
+                    />
+                    <CardContent >
+                        <Box display="flex" justify="space-between">
+                        <Typography sx={{justifyContent:"center", display:"flex"}} gutterBottom variant="body">{listing.item.item_name}</Typography>
+                        </Box>
+                    </CardContent>
+                </Card>
         )
     }
 
@@ -52,15 +76,25 @@ export default function AllListings(props) {
 
     return (
         <div className='listing'>
+            <Grid
+            container
+            width="100%"
+            direction="row"
+            justifyContent="center"
+            alignItems="center" 
+            xl={12}
+            spacing={1}
+          >
         
-            {listings?.map(listing => (
-                searchValue
-                    ?
-                    listing.item.item_name.includes(searchValue.toLowerCase()) && showListing(listing)
-                    :
-                    showListing(listing)
-            ))
-            }
+                {listings?.map(listing => (
+                    searchValue
+                        ?
+                        listing.item.item_name.includes(searchValue.toLowerCase()) && showListing(listing)
+                        :
+                        showListing(listing)
+                ))
+                }
+            </Grid>
        
         </div>
     )
