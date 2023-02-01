6e6c0975f5bf6c381214febec6df9b6a8620ff87
Adam Burrough <adamjamesburrough@gmail.com>
2023-02-02 00:30:04 +0900

    depolyment preperation

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from backend.settings import BASE_DIR
+import os
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("api.urls")),
+    path('', TemplateView.as_view(template_name="index.html"))
 ]
+
+print("😁",(
+    os.path.join(BASE_DIR,  'build', 'static')
+    # BASE_DIR / "static/"
+))
