e7ef903c12d603c6bedaed135cbad3e5223696e7
Adam Burrough <adamjamesburrough@gmail.com>
2023-02-03 22:01:54 +0900

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -97,11 +97,10 @@ ALLOWED_HOSTS = ['*']
 #         'PORT': env("DB_PORT"),
 #     }
 # }
-DATABASES['default'] = dj_database_url.parse(
-    os.environ.get("DATABASE_URL"),
-    conn_max_age=600,
-    conn_health_checks=True,
-)
+DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASES = {
+    'default': dj_database_url.config(),
+}
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
