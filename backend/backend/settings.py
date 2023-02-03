e4feabd52a7bdd0d66a4d67af6a50bef867c1b29
Adam Burrough <adamjamesburrough@gmail.com>
2023-02-03 20:57:06 +0900

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 CORS_ORIGIN_ALLOW_ALL = True
 
