2ef0814d35e5582850c527a5f9759dc1a4e9ca29
Adam Burrough <adamjamesburrough@gmail.com>
2023-02-10 00:35:04 +0900

    test2

--- src/componenet/AllListings.js
+++ src/componenet/AllListings.js
@@ -9,7 +9,7 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const BASE_URL = 'http://127.0.0.1:8000/api';
+const BASE_URL = process.env.REAT_APP_PROXY_URL;
 
 console.log(process.env.PROXY_URL)
 
