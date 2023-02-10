7d3a4d71d910da0f853bf71d578cc3d20144bdc9
Adam Burrough <adamjamesburrough@gmail.com>
2023-02-10 18:47:35 +0900

    revert

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,17 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from django.views.static import serve
-from django.urls import re_path
-import settings
+
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("api.urls")),
     path('', TemplateView.as_view(template_name="index.html")),
-     re_path(r'^media/(?P<path>.*)$', serve, {
-        'document_root': settings.MEDIA_ROOT,
-    }),
 ]
 
